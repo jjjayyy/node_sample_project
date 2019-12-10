f406f7b040de13df5eb69596a2f27a971d0c870b
jh <hwinnovation@naver.com>
2019-12-11 00:05:03 +0900

    node mysql connection ing

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/*
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host : 'localhost',
-  user : '<root>'
-})
+  user : process.env.MYSQL_ID,
+  password : process.env.MYSQL_PW,
+  port : 3306,
+  database : 'mysql'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM SAMPLE', function(err, rows, fields) {
+  if(!err)
+    console.log('The solution is : ', rows);
+  else
+    console.log('Error while performing Query.', err);
+});
+
+connection.end();
+*/
+console.log("ID : " + process.env.MYSQL_ID);
+console.log("PW : " + process.env.MYSQL_PW);
+console.log("NODE : " + process.env.NODE_ENV);
+router.get('/', (req, res) =>  res.send("pw ::: " + process.env.MYSQL_PW));
+
+module.exports = router;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+module.exports = router;*/
