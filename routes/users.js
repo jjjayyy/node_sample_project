d50f2213c6981e2e7409a0f337af6fca1b00088e
jh <hwinnovation@naver.com>
2019-12-12 23:26:25 +0900

    node mysql connection test

--- routes/users.js
+++ routes/users.js
@@ -15,10 +15,3 @@ router.get('/',(req,res) =>
 }));
 
 module.exports = router;
-
-/* GET users listing. */
-/*router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;*/
